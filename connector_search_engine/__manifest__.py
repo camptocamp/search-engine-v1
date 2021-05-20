172ae348fdf1d6ac2fc1a5c1147579d5df9dc3eb
Simone Orsi <simahawk@gmail.com>
2021-05-20 16:06:20 +0200

    [14.0] connector_search_engine: Migration to v14

--- connector_search_engine/__manifest__.py
+++ connector_search_engine/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Connector Search Engine",
-    "version": "13.0.2.6.1",
+    "version": "14.0.1.0.0",
     "author": "Akretion,"
     "ACSONE SA/NV,"
     "Camptocamp,"
@@ -35,5 +35,5 @@
         'data/ir_cron.xml',
         'security/ir.model.access.csv',
     ],
-    "installable": False,
+    "installable": True,
 }

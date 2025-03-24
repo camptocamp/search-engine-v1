OCA/search-engine

pre-commit, black OCA conventions

connector_search_engine/tests/models_mixin.py
@@ -68,18 +68,13 @@ class TestMixin(object):
             # here we must remove the model from list of children of inherited
             # models
             parents = cls._inherit
-            parents = (
-                [parents]
-                if isinstance(parents, basestring)
-                else (parents or [])
-            )
+            parents = [parents] if isinstance(parents, basestring) else (parents or [])
             # keep a copy to be sure to not modify the original _inherit
             parents = list(parents)
             parents.extend(cls._inherits.keys())
             parents.append("base")
             funcs = [
-                attrgetter(kind + "_children")
-                for kind in ["_inherits", "_inherit"]
+                attrgetter(kind + "_children") for kind in ["_inherits", "_inherit"]
             ]
             for parent in parents:
                 for func in funcs:
@@ -90,9 +85,7 @@ class TestMixin(object):
                         children.remove(cls._name)
 
     def _test_get_model_id(self):
-        self.env.cr.execute(
-            "SELECT id FROM ir_model WHERE model = %s", (self._name,)
-        )
+        self.env.cr.execute("SELECT id FROM ir_model WHERE model = %s", (self._name,))
         res = self.env.cr.fetchone()
         return res[0] if res else None
 

camptocamp/search-engine-v1

connector_search_engine/tests/models_mixin.py
@@ -61,16 +61,13 @@ class TestMixin(object):
             # here we must remove the model from list of children of inherited
             # models
             parents = cls._inherit
-            parents = (
-                [parents] if isinstance(parents, str) else (parents or [])
-            )
+            parents = [parents] if isinstance(parents, str) else (parents or [])
             # keep a copy to be sure to not modify the original _inherit
             parents = list(parents)
             parents.extend(cls._inherits.keys())
             parents.append("base")
             funcs = [
-                attrgetter(kind + "_children")
-                for kind in ["_inherits", "_inherit"]
+                attrgetter(kind + "_children") for kind in ["_inherits", "_inherit"]
             ]
             for parent in parents:
                 for func in funcs:
@@ -81,9 +78,7 @@ class TestMixin(object):
                         children.remove(cls._name)
 
     def _test_get_model_id(self):
-        self.env.cr.execute(
-            "SELECT id FROM ir_model WHERE model = %s", (self._name,)
-        )
+        self.env.cr.execute("SELECT id FROM ir_model WHERE model = %s", (self._name,))
         res = self.env.cr.fetchone()
         return res[0] if res else None
 
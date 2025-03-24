OCA/search-engine

[12.0] add settings export

connector_search_engine/components/exporter.py
@@ -29,3 +29,7 @@ class SeExporter(Component):
             return self._index(
                 [record.get_export_data() for record in self.work.records]
             )
+
+    def export_settings(self, force=True):
+        """ Run the settings synchronization """
+        return self.backend_adapter.set_settings(force=force)

camptocamp/search-engine-v1

connector_search_engine/components/exporter.py
@@ -29,3 +29,7 @@ class SeExporter(Component):
             return self._index(
                 [record.get_export_data() for record in self.work.records]
             )
+
+    def export_settings(self, force=True):
+        """ Run the settings synchronization """
+        return self.backend_adapter.settings(force=force)
shouyejing/connector-nosql

[REF] rename module as there are used for indexing data not for nosql (search engine use concept of index, nosql do not have this need)

connector_algolia/unit/exporter.py
@@ -1,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2016 Akretion (http://www.akretion.com)
+# Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+from openerp.addons.connector.queue.job import job
+from openerp.addons.connector_se.unit.exporter import (
+    export_record_se,
+    SeExporter)
+from openerp.addons.connector_se_algolia.backend import algolia
+
+
+@job(default_channel='root.algolia.product')
+def export_record(session, model_name, binding_ids):
+    return export_record_se(session, model_name, binding_ids)
+
+
+@algolia
+class AlgoliaExporter(SeExporter):
+    _model_name = 'se.product.template'
+
+    def get_export_func(self):
+        """ Should return the delay export func of the binding """
+        return export_record

camptocamp/search-engine-v1

connector_algolia/unit/exporter.py
@@ -1,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2016 Akretion (http://www.akretion.com)
+# Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+from openerp.addons.connector.queue.job import job
+from openerp.addons.connector_se.unit.exporter import (
+    export_record_se,
+    SeExporter)
+from openerp.addons.connector_se_algolia.backend import algolia
+
+
+@job(default_channel='root.algolia.product')
+def export_record(session, model_name, binding_ids):
+    return export_record_se(session, model_name, binding_ids)
+
+
+@algolia
+class AlgoliaExporter(SeExporter):
+    _model_name = 'se.product.product'
+
+    def get_export_func(self):
+        """ Should return the delay export func of the binding """
+        return export_record